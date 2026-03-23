af90640b3e2cd23375a8cf98ce098eb31d637ff4
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-24 00:57:09 +0100

    fix views import path

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,8 +2,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from backend.users.models import User
-from backend.users.serializers import UserRegisterSerializer
+from .models import User
+from .serializers import UserRegisterSerializer
 
 
 class RegisterView(generics.CreateAPIView):
