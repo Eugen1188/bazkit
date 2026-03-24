cbe11daf18689247ad5b1086f6df67e020447b5c
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-24 02:07:18 +0100

    fix route

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from "../../node_modules/@angular/router/index";
-import { LoginComponent } from "./login/login.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
