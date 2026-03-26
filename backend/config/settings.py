c08fd5a570a4518fafffbd611869372c717f50ae
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-27 00:58:59 +0100

    add gitignore file, add connection to .env file for database pw

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+SETTINGS_DIR = Path(__file__).resolve().parent
+BACKEND_DIR = SETTINGS_DIR.parent
+PROJECT_ROOT = BACKEND_DIR.parent
+
+load_dotenv(PROJECT_ROOT / ".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,12 +103,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+   "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "bazkit"),
-        "USER": os.getenv("POSTGRES_USER", "bazkit"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "bazkit"),
-        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
         "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
