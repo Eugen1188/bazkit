ab69ff184c979b987ccf0385e47968adca790122
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-31 00:24:07 +0200

    fix urls for reigsteruser

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import RegisterView, UserDetailView, UserListView
+from .views import RegisterUserView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("me/", UserDetailView.as_view(), name="user-detail"),
-    path("user-list/", UserListView.as_view(), name="user-list"),
+    path("register/", RegisterUserView.as_view(), name="register"),
     ]
