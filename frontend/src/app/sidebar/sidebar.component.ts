47273c04f22d4c5c59ac9611dcd9fbb8a27470eb
Eugen1188 <eugen.ferchow@gmx.de>
2026-06-20 20:43:40 +0200

    update navbar

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,24 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
-  userName = 'Eugen';
-
-  navItems = [
-    // { label: 'Übersicht', route: '/main/overview', icon: 'home' },
-    { label: 'Einkaufsliste', route: '/main/shopping-list', icon: 'list' },
-    { label: 'Rezepte', route: '/main/recipe-list', icon: 'recipe' },
-    // { label: 'Planer', route: '/main/planner', icon: 'calendar' },
-    { label: 'Gespeichert', route: '/main/saved-list', icon: 'bookmark' },
-    // { label: 'Profil', route: '/main/profile', icon: 'user' },
-    { label: 'Einstellungen', route: '/main/settings', icon: 'settings' },
-  ];
-}
+export class SidebarComponent {}
