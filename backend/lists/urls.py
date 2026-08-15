74fe553ef2722b6b52088e82b67ec07d9ab8f934
Eugen Ferchow <eugen.ferchow@gmx.de>
2026-08-15 02:16:23 +0200

    add note field migration

--- backend/lists/urls.py
+++ backend/lists/urls.py
@@ -3,24 +3,25 @@ from django.urls import path
 from .views import (
     SavedListListCreateAPIView,
     SavedListDetailAPIView,
-    SavedListItemSerializer
+    SavedListItemDetailAPIView,
 )
 
 urlpatterns = [
     path(
         "saved-lists/",
         SavedListListCreateAPIView.as_view(),
-        name="saved-list-list-create"
+        name="saved-list-list-create",
     ),
 
     path(
         "saved-lists/<int:pk>/",
         SavedListDetailAPIView.as_view(),
-        name="saved-list-detail"
+        name="saved-list-detail",
     ),
+
     path(
-    "saved-lists/<int:list_id>/items/<int:item_id>/",
-    SavedListItemDetailAPIView.as_view(),
-    name="saved-list-item-detail"
+        "saved-lists/<int:list_id>/items/<int:item_id>/",
+        SavedListItemDetailAPIView.as_view(),
+        name="saved-list-item-detail",
     ),
 ]
