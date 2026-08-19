0c988aef282be4574492861487357bff25b0a5bb
Eugen Ferchow <eugen.ferchow@gmx.de>
2026-08-19 21:50:26 +0200

    add product autocomplete

--- backend/products/models.py
+++ backend/products/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Product(models.Model):
+
     name = models.CharField(
         max_length=100,
-        db_index=True
+        unique=True
     )
 
     category = models.CharField(
@@ -16,24 +17,6 @@ class Product(models.Model):
         max_length=30,
         blank=True
     )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-    class Meta:
-        ordering = [
-            "name"
-        ]
-
-        constraints = [
-            models.UniqueConstraint(
-                fields=[
-                    "name"
-                ],
-                name="unique_product_name"
-            )
-        ]
 
     def __str__(self):
         return self.name
